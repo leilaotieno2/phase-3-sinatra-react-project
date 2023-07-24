7dc6b8cc7a60ff524def8516c2f63468b8fe5841
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-07-24 13:26:38 +0300

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,36 +1,25 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+  # Require necessary gems and libraries
+  require 'sinatra/base'
+  require 'sinatra/activerecord'
 
-# Require necessary gems and libraries
-require 'sinatra/base'
-require 'sinatra/activerecord'
+  # Require your controllers
+  require_relative 'patients_controller'
+  require_relative 'appointments_controller'
+  require_relative 'doctors_controller'
+  require_relative 'departments_controller'
+  require_relative 'categories_controller'
+  require_relative 'invoices_controller'
 
-# Require your controllers
-require_relative '/home/leila/development/code/phase-3/phase-3-sinatra-react-project/app/controllers/patients_controller.rb'
-require_relative '/home/leila/development/code/phase-3/phase-3-sinatra-react-project/app/controllers/appointments_controller.rb'
-require_relative '/home/leila/development/code/phase-3/phase-3-sinatra-react-project/app/controllers/doctors_controller.rb'
-require_relative '/home/leila/development/code/phase-3/phase-3-sinatra-react-project/app/controllers/departments_controller.rb'
-require_relative '/home/leila/development/code/phase-3/phase-3-sinatra-react-project/app/controllers/categories_controller.rb'
-require_relative '/home/leila/development/code/phase-3/phase-3-sinatra-react-project/app/controllers/invoices_controller.rb'
+  # Your application settings and configuration can go here
 
-# Configure database connection
-# Set your database configuration based on your database setup
-# Example: ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'your_database.db')
+  # Optionally, you can add some helpers or middlewares here
 
-# Your application settings and configuration can go here
+  # Optionally, you can also set a layout or other settings here
 
-# Optionally, you can add some helpers or middlewares here
-
-# Optionally, you can also set a layout or other settings here
-
-# Run the application
-# Only if running the app directly (not when running tests)
-# run! if __FILE__ == $0
-  # GET route to fetch all appointments
-  
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
-
+  # Run the application
+  # Only if running the app directly (not when running tests)
+  # run! if __FILE__ == $0
 end
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,40 +1,37 @@
-# app/controllers/appointments_controller.rb
-
 class AppointmentsController < Sinatra::Base
-    set :default_content_type, 'application/json'
-    
-    # GET route to fetch all appointments
-    get "/appointments" do
-      appointments = Appointment.all
-      appointments.to_json
-    end
-  
-    # POST route to create a new appointment
-    post "/appointments" do
-      appointment = Appointment.create(
-        appointment_date: params[:appointment_date],
-        start_time: params[:start_time],
-        # Add other attributes for the Appointment model here
-      )
-      appointment.to_json
-    end
-  
-    # PUT route to update an existing appointment
-    put "/appointments/:id" do
-      appointment = Appointment.find(params[:id])
-      appointment.update(
-        appointment_date: params[:appointment_date],
-        start_time: params[:start_time],
-        # Add other attributes for the Appointment model here
-      )
-      appointment.to_json
-    end
-  
-    # DELETE route to delete an appointment
-    delete "/appointments/:id" do
-      appointment = Appointment.find(params[:id])
-      appointment.destroy
-      { message: "Appointment deleted successfully!" }.to_json
-    end
+  set :default_content_type, 'application/json'
+
+  # GET route to fetch all appointments
+  get "/appointments" do
+    appointments = Appointment.all
+    appointments.to_json
   end
-  
+
+  # POST route to create a new appointment
+  post "/appointments" do
+    appointment = Appointment.create(
+      appointment_date: params[:appointment_date],
+      start_time: params[:start_time],
+      # Add other attributes for the Appointment model here
+    )
+    appointment.to_json
+  end
+
+  # PUT route to update an existing appointment
+  put "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.update(
+      appointment_date: params[:appointment_date],
+      start_time: params[:start_time],
+      # Add other attributes for the Appointment model here
+    )
+    appointment.to_json
+  end
+
+  # DELETE route to delete an appointment
+  delete "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    { message: "Appointment deleted successfully!" }.to_json
+  end
+end
